61f44778d41c607cdf719df9a7f7127098229f45
Maksim11011 <mvbspb@bk.ru>
2024-03-12 11:40:42 +0300

    init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'widgets/app/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   const app = MyApp();
   runApp(app);
 }
